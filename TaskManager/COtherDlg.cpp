8c8f925977df40041e69e1d3091e9d2dd0b7e831
ry1ynn <ry1ynn_sec@163.com>
2019-09-06 13:48:21 +0800

    初建项目，并添加Tab控件

---
+++ TaskManager/COtherDlg.cpp
@@ -0,0 +1,34 @@
+﻿// COtherDlg.cpp: 实现文件
+//
+
+#include "stdafx.h"
+#include "TaskManager.h"
+#include "COtherDlg.h"
+#include "afxdialogex.h"
+
+
+// COtherDlg 对话框
+
+IMPLEMENT_DYNAMIC(COtherDlg, CDialogEx)
+
+COtherDlg::COtherDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG2, pParent)
+{
+
+}
+
+COtherDlg::~COtherDlg()
+{
+}
+
+void COtherDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(COtherDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// COtherDlg 消息处理程序
---
+++ TaskManager/CProcessDlg.cpp
@@ -0,0 +1,34 @@
+﻿// CProcessDlg.cpp: 实现文件
+//
+
+#include "stdafx.h"
+#include "TaskManager.h"
+#include "CProcessDlg.h"
+#include "afxdialogex.h"
+
+
+// CProcessDlg 对话框
+
+IMPLEMENT_DYNAMIC(CProcessDlg, CDialogEx)
+
+CProcessDlg::CProcessDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+CProcessDlg::~CProcessDlg()
+{
+}
+
+void CProcessDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CProcessDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CProcessDlg 消息处理程序
